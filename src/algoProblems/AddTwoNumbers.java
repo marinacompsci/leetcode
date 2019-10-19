8a23a94bf7d26642f4571a57e1fe24e36329e240
Marina A <25265018+MarinaBSA@users.noreply.github.com>
2019-10-19 19:18:58 +0200

    finished - bug free

--- src/algoProblems/AddTwoNumbers.java
+++ src/algoProblems/AddTwoNumbers.java
@@ -1,36 +1,115 @@
 package algoProblems;
+
 import java.util.ArrayList;
+import java.lang.Math;
 
 public class AddTwoNumbers {
 	public static void main(String[] args) {
-		ArrayList<AddTwoNumbers.ListNode> nodes = new ArrayList<AddTwoNumbers.ListNode>();
-		ListNode ln1 = new ListNode(2);
-		ListNode ln2 = new ListNode(4);
-		ListNode ln3 = new ListNode(3);
-		ln1.next = ln2;
-		ln2.next = ln3;
+		ArrayList<AddTwoNumbers.ListNode> nodes1 = new ArrayList<AddTwoNumbers.ListNode>();
+		ListNode ln11 = new ListNode(5);
+		ListNode ln21 = new ListNode(8);
+		ListNode ln31 = new ListNode(3);
+		ln11.next = ln21;
+		ln21.next = ln31;
+		
+		ArrayList<AddTwoNumbers.ListNode> nodes2 = new ArrayList<AddTwoNumbers.ListNode>();
+		ListNode ln12 = new ListNode(5);
+		ListNode ln22 = new ListNode(6);
+		ListNode ln32 = new ListNode(4);
+		ln12.next = ln22;
+		ln22.next = ln32;
 	
-		nodes.add(ln1);
-		nodes.add(ln2);
-		nodes.add(ln3);
-		//ln3.next = null;
-		System.out.println(nodes.get(2).val);
-//		for(int i = 0; i < 3; i++) {
-//			System.out.println(nodes[]);
-//		}
+		System.out.println(walkNodes(addTwoNumbers(ln11, ln12)).toString());		
 	}
 	
+	public static ListNode addTwoNumbers(ListNode nodeList1, ListNode nodeList2) {
+		ArrayList<Integer> nodeListA = walkNodes(nodeList1);
+		ArrayList<Integer> nodeListB = walkNodes(nodeList2);
+		int sum = 0;
+		int remainder = 0;
+		int nodeListAVal = 0;
+		int nodeListBVal = 0;
+		int amountOfNodes = Math.max(nodeListA.size(), nodeListB.size());
+		ListNode node = null;
+		ListNode currentNode = null;
+		
+		for (int i = 0; i < amountOfNodes; i++) {
+			ListNode newNode = null;
+			try {
+				nodeListAVal = nodeListA.get(i);				
+			} catch(Exception e) {
+				//array out of bounds
+				nodeListAVal = 0;
+			}
+			
+			try {
+				nodeListBVal = nodeListB.get(i);				
+			} catch(Exception e) {
+				//array out of bounds
+				nodeListBVal = 0;
+			}			
+			sum = nodeListAVal + nodeListBVal + remainder; //actual node values plus remainder from last 2 nodes				
+			remainder = splitDigits(sum)[0]; 
+			sum = splitDigits(sum)[1];
+			if(i == 0) {
+			//create new node
+				node = new ListNode(sum);					
+				currentNode = node;
+			} else {
+				//append a new node to the last one
+				newNode = new ListNode(sum);
+				currentNode.next = newNode;
+				currentNode = newNode;
+			}
+			//debug
+			//System.out.print("res: " + currentNode.val + " ");
+		}
+		
+		//create new nodes for the remaining numbers after there are no more nodes from the input left to walk
+		//attach them to the last node created
+		while(remainder != 0) {
+			ListNode newNode = null;
+			sum = splitDigits(remainder)[1];			
+			remainder = splitDigits(remainder)[0]; 
+			newNode = new ListNode(sum);
+			currentNode.next = newNode;
+			currentNode = newNode;
+			//debug
+			//System.out.println("last: " + currentNode.val + " ");
+		}
+		
+		return node;
+	}
+
+	
+	public static ArrayList<Integer> walkNodes(ListNode list) {
+		int value;
+		ArrayList<Integer> nodesArray = new ArrayList<Integer>();
+		while(list != null) {
+			value = list.val;
+			nodesArray.add(value);
+			//System.out.println("test: " + value);
+			list = list.next;
+		}
+		return  nodesArray;
+	}
+	
+	//7 -> 7 / 10 = 0 , R = 7 ==> [0, 7]
+	//10 -> 10 / 10  = 1, R = 0 ==> [1, 0]
+	public static int[] splitDigits(int num) {
+		int firstDigit = num / 10;
+		int secDigit = num % 10;
+		int[] result = {firstDigit, secDigit}; 
+		return result;
+	}
+		
 	private static class ListNode {
-		 int val;
-		 ListNode next;
-		 ListNode(int x) { val = x; }	//constructor
+		int val;
+		ListNode next;
+		ListNode(int x) { val = x; }	//constructor
 	}
+	
 }
-
-
-
-
-
 
 
 /**
